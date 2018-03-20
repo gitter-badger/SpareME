e041d1ac844c2ede3014b09fd9e31836a4226748
Patrick Gatewood <pg8wood@vt.edu>
2018-03-20 01:08:26 -0400

    Add "/add" routine for single string
    POST text and category to API

--- SpareMe/network/ml-api.js
+++ SpareMe/network/ml-api.js
@@ -1,8 +1,7 @@
-const ADD = "add?";
+const ADD = "add";
 const BASE_URL = "http://ec2-18-188-74-206.us-east-2.compute.amazonaws.com:5000/";
 const DEFAULT_CATEGORY = "harmless";
 const PREDICT = "predict?text=";
-
 
 /**
  * Returns the String category for the passed-in tokens from the API.
@@ -19,9 +18,13 @@ export function getCategoryForWords() {
         return response._bodyText;
     });
 }
-
+/**
+ * Returns the String category for an element's inner (plain) text.
+ * Note: for more accurate results, try to use the most deeply-nested
+ * element possible (don't call this with `HTML` or `body`).
+ */
 export function getCategoryForHtmlElement(element) {
-    let phrase = element.innerHTML;
+    let phrase = element.innerText;
 
     if (phrase) {
         let url = BASE_URL + PREDICT + phrase.split(' ').join('%');
@@ -31,5 +34,25 @@ export function getCategoryForHtmlElement(element) {
         });
     }
 
+    return DEFAULT_CATEGORY;
+}
 
+export function addTextToCategory(text, category) {
+    let url = BASE_URL + ADD;
+
+    var form = new FormData();
+    form.append('text', text);
+    form.append('category', category);
+
+    let requestData = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        },
+        body: form
+    }
+
+    fetch(url, requestData).then(function(response) {
+        return response._bodyText;
+    });
 }
