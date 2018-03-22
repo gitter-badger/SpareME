883f1b547f2b6f15dbec34bee28ec539b187ce16
Patrick Gatewood <pg8wood@vt.edu>
2018-03-22 16:06:38 -0400

    Encapsulate injected JavaScript
    Removes that monster script from the FilteredWebView class and
    locks it away in its own file
    
    Improves readability and modularity for resuse down the line

---
+++ SpareMe/components/injected.js
@@ -0,0 +1,73 @@
+/**
+ * Self-executing function to inject into the WebView
+ */
+export const injectedJS = `(${String(function() {
+    const HTML_TEXT_ELEMENTS  = ['p', 'a', 'li', 'img'];
+    const INJECTED_CLASSNAME = "SpareMeElement";
+    var injectedClassCounter = 0;
+
+    inject();
+
+    function inject() {
+        // Open two-way message channel between React and the WebView
+        createMessageSender();
+        document.addEventListener('message', onReactMessage);
+
+        // Send tags to React for processing
+        analyzePage();
+    }
+
+    /**
+     * Sends messages from the WebView to React
+     */
+    function createMessageSender() {
+        // Steal back any site's postMessage overrides 😏
+        var originalPostMessage = window.postMessage;
+        var patchedPostMessage = function(message, targetOrigin, transfer) {
+            originalPostMessage(message, targetOrigin, transfer);
+        }
+        patchedPostMessage.toString = function() {
+            return String(Object.hasOwnProperty).replace('hasOwnProperty', 'postMessage');
+        }
+        window.postMessage = patchedPostMessage;
+    }
+
+    /**
+     * Handles messages from React
+     */
+    function onReactMessage(data) {
+        let action = JSON.parse(data.data);
+        let name = action['name'];
+        let className = action['className'];
+
+        if (name === 'hide') {
+            hideElement(document.getElementsByClassName(className)[0]);
+        }
+    }
+
+    function hideElement(elementToHide) {
+        elementToHide.style.filter = 'blur(10px)';
+        elementToHide.style.color = 'transparent';
+        elementToHide.style.textShadow = '0 0 5px rgba(0,0,0,0.5)';
+    }
+
+    function analyzePage() {
+        for (let htmlElementName of HTML_TEXT_ELEMENTS) {
+            var elements = document.getElementsByTagName(htmlElementName);
+            for (let element of elements) {
+                // Add unique class so we can find this element later
+                let addedClass = INJECTED_CLASSNAME + injectedClassCounter;
+                injectedClassCounter += 1;
+                element.classList.add(addedClass);
+
+                // Send innerText to React
+                window.postMessage(JSON.stringify({
+                    messageType: 'predict',
+                    content : String(htmlElementName === 'img' ?  element.alt : element.innerText),
+                    addedClass: addedClass
+                }));
+            }
+        }
+    }
+
+})})();` // JavaScript :)
