f9b4d72df24c85aa4a59bbf6618a77b2968adad6
Patrick Gatewood <pg8wood@vt.edu>
2018-03-22 23:45:51 -0400

    Speed up element hiding
    Removes <div> hiding for demo 1; speeds up hiding for other
    elements

--- SpareMe/components/injected.js
+++ SpareMe/components/injected.js
@@ -51,63 +51,20 @@ export const injectedJS = `(${String(function() {
     }
 
     function analyzePage() {
-        var element;
-        var iterator = document.createNodeIterator(
-            document.body,
-            NodeFilter.SHOW_ELEMENT,
+        var elements = document.body.querySelectorAll('p, a, li, span');
+        for (let element of elements) {
+            // Add unique class so we can find this element later
+            let addedClass = INJECTED_CLASSNAME + injectedClassCounter;
+            injectedClassCounter += 1;
+            element.classList.add(addedClass);
 
-            { acceptNode: function(node) {
-                let hidable  = ['p', 'a', 'li', 'img', 'div'];
-                let tag = node.tagName.toLowerCase();
-                console.log(tag);
-                if (hidable.indexOf(tag) > -1) {
-                    if (tag === 'div') {
-                        if (node.hasChildNodes()) {
-                            console.log('rejecting ' + tag)
-                            return NodeFilter.FILTER_REJECT;
-                        }
-                    }
-                    console.log('accepting ' + tag)
-                    return NodeFilter.FILTER_ACCEPT
-                } else {
-                    console.log('tag "' + tag + '" not in hideable')
-                }
-            }
-        });
-        // var elements = Array.from(document.body.querySelectorAll('*'));
-        // for (let element of elements) {
-
-        console.log('looking at nodes');
-        while (element = iterator.nextNode()) {
-            console.log(element)
-                console.log('looking at element ' + element)
-                // Add unique class so we can find this element later
-                let addedClass = INJECTED_CLASSNAME + injectedClassCounter;
-                injectedClassCounter += 1;
-                element.classList.add(addedClass);
-
-                // Send innerText to React
-                window.postMessage(JSON.stringify({
-                    messageType: 'predict',
-                    //content : String(element.innerText),
-                    content : String(element.tagName === 'img' ? element.alt : element.innerText),
-                    addedClass: addedClass
-                }));
-            }
-
-        // }
+            // Send innerText to React
+            window.postMessage(JSON.stringify({
+                messageType: 'predict',
+                content : String(element.tagName === 'img' ? element.alt : element.innerText),
+                addedClass: addedClass
+            }));
+        }
     }
-
-
-
-
-    /**
-     * Get HTML elements that contain text and aren't wrapper/container elements
-     */
-    // function getHideableElements() {
-    //     return Array.from(document.body.querySelectorAll('*')).filter((elem) => {
-    //         return elem in HIDEABLE_ELEMENTS || elem.children.length === 0;
-    //     });
-    // }
 
 })})();` // JavaScript :)
