bb7bb799f29dfefce917b09df4edec0bbf24c4ce
Travis Weissenberger <wtravis1@vt.edu>
2018-03-25 12:36:03 -0400

    Added simple signin page utalizing stack navigator

--- SpareMe/App.js
+++ SpareMe/App.js
@@ -1,13 +1,14 @@
 'use strict';
 import React, { Component } from 'react';
-import { StyleSheet, WebView, Text, ActivityIndicator, View } from 'react-native';
+import { StyleSheet, WebView, Text, ActivityIndicator, View, Button, TextInput } from 'react-native';
 import CustomStatusBar from './components/CustomStatusBar'
 import URLBar from './components/URLBar'
 import * as api from 'ml-api'
 import * as constants from 'constants'
 import FilterWebView from './components/FilterWebView'
+import { StackNavigator } from 'react-navigation'
 
-export default class App extends Component {
+class HomeScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {url: 'https://www.google.com/'};
@@ -66,18 +67,87 @@ export default class App extends Component {
                     refreshHandler={this.refresh}
                     onChangeHandler={this.textChangeHandler}
                     url={this.state.url}
-                    onRef={ref => (this.urlBar = ref)}
-                />
+                    onRef={ref => (this.urlBar = ref)}/>
                 <FilterWebView
                     source={{uri: this.state.url}}
                     javaScriptEnabledAndroid={true}
                     onNavigationStateChange={this.navChangeHandler}
                     onError={this.webErrorHandler}
                     renderError={this.renderError}
-                    onRef={ref => (this.webView = ref)}
-                />
+                    onRef={ref => (this.webView = ref)}/>
+                <Button
+                    title="Log In"
+                    onPress={() => this.props.navigation.navigate('SignIn')}/>
             </View>
         );
+    }
+}
+
+class SignInScreen extends Component {
+    render() {
+        return (
+            <View style={styles.loginView}>
+                <Text style={{fontSize: 30}}>
+                    Sign In
+                </Text>
+                <Text style={{fontSize: 20, paddingTop: 20}}>
+                    Username:
+                </Text>
+                <TextInput
+                    style= {{width: 150}}
+                    placeholder="Enter Username"
+                />
+                <Text style={{fontSize: 20}}>
+                    Password:
+                </Text>
+                <TextInput
+                    style= {{width: 150}}
+                    placeholder="Enter Password"
+                />
+
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button
+                            title='Sign In'
+                            onPress={() => this.props.navigation.goBack()}
+                        />
+                    </View>
+                    <View style={styles.button}>
+                        <Button
+                            title='Cancel'
+                            backgroundColor={'red'}
+                            onPress={() => this.props.navigation.goBack()}
+                        />
+                    </View>
+                </View>
+                
+            </View>
+            
+        );
+    }
+}
+
+const RootStack = StackNavigator(
+    {
+        Home: {
+            screen: HomeScreen,
+        },
+        SignIn: {
+            screen: SignInScreen,
+        },
+    },
+    {
+        initialRouteName: 'Home',
+        headerMode: 'none',
+        navigationOptions: {
+            headerVisible: false,
+        }
+    }
+);
+
+export default class App extends Component {
+    render() {
+        return <RootStack />;
     }
 }
 
@@ -89,5 +159,18 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         flex: 1
+    },
+    loginView: {
+        padding: 50,
+        flex: 1
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    button: {
+        width: '40%',
+        height: 40
     }
 });
