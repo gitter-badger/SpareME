8d4bc53bfadcdb24868e45eb3c96e3c5bd2668df
benbrott <ben.r.brott@gmail.com>
2018-04-16 18:20:29 -0400

    Add SplashScreen support on iOS
    Added dependencies to support react-native-splash-screen, screen not yet
    designed.

--- SpareMe/ios/SpareMe/AppDelegate.m
+++ SpareMe/ios/SpareMe/AppDelegate.m
@@ -12,6 +12,7 @@
 #import <Firebase.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "SplashScreen.h"
 
 @implementation AppDelegate
 
@@ -33,6 +34,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [SplashScreen show];
   return YES;
 }
 
